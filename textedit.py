956f62d6ba48e81f99a7e71746bc6ae94bc627ec
hhhhsc_701 <1710496817@qq.com>
2020-03-21 15:42:46 +0800

    initial update

---
+++ textedit.py
@@ -0,0 +1,10 @@
+#文本编辑器
+import re
+import webbrowser
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+from tkinter import *
+from tkinter import filedialog
+from tkinter.scrolledtext import ScrolledText
+import tkinter.messagebox as tm
