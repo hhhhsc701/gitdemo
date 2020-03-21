2add4127edee59bf529ae1e25cd7b5ae8eb06c9d
hhhhsc_701 <1710496817@qq.com>
2020-03-21 15:55:08 +0800

    add stop list

--- textedit.py
+++ textedit.py
@@ -25,3 +25,19 @@ def get_content(path):
             content += l
         line = re.sub(re_punctuation,"",content.lower())
         return line.split()
+
+
+#将停用词表转换为列表
+def stop_word():
+	file=open(r'E:\文档\大作业\英文停用词表.txt').readlines()
+	stop_list=[i.replace(' \n','') for i in file]
+	return stop_list
+
+#去除停用词
+def remove_stop(seg_list):
+    new_list=[]
+    stop_list=stop_word()
+    for word in seg_list:
+        if word not in stop_list:
+            new_list.append(word)
+    return new_list
