2ec5c51aebfdbee61f1fb0bdad0955accfbde0ac
hhhhsc_701 <1710496817@qq.com>
2020-03-21 15:50:23 +0800

    update window

--- textedit.py
+++ textedit.py
@@ -8,3 +8,20 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter.scrolledtext import ScrolledText
 import tkinter.messagebox as tm
+
+#创建界面
+root = Tk()
+root.geometry('600x500')
+root.title('文本编辑器')
+
+#去标点并转换小写,除去单词间的空格并以列表形式输出
+def get_content(path):  
+    punctuation='!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~…'
+    re_punctuation = "[{}]+".format(punctuation)
+    with open(path,'r',encoding='gbk',errors='ignore')as f:
+        content=''
+        for l in f:
+            l.strip()
+            content += l
+        line = re.sub(re_punctuation,"",content.lower())
+        return line.split()
